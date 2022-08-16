db5b3e0f135073eae5768625f77a4fef4345b8e2
nthigadenis <denno@DESKTOP-TK85V7G.localdomain>
2022-08-16 15:18:32 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2;
+let multiply;
+
+multiply = num1 * num2
+
+let random = Math.ceil(Math.random());
+
+
+let num3 = 24
+let num4 = 5
+let mod;
+mod = num3 % num4
+
+
+let max;
+max = Math.max(4, 7, 9, 13, 20, 1)
+
+
+
+
+
